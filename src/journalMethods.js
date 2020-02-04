2300b99908efbcbdfd6f7b0d684a30862136d540
elliot jordan and patrick delaney <elliot+patrick@>
2020-02-03 16:48:35 -0800

    npm fun - altering prototypes

---
+++ src/journalMethods.js
@@ -0,0 +1,45 @@
+
+export function JournalLog() {
+  this.journals = [];
+  this.currentId = 0;
+}
+export function Journal(title, body) {
+  this.title = title;
+  this.body = body;
+}      
+
+JournalLog.prototype.addJournal = function(journal) {
+  journal.id = this.assignId();
+  this.journals.push(journal);
+};
+
+JournalLog.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+Journal.prototype.wordCount = function() {
+  return this.body.split(" ").length;
+};
+
+Journal.prototype.vowelCount = function() {
+  var totalLetters = this.body.split("");
+  var vow = ["a", "i", "u", "e", "o"];
+  var totalVows = [];
+  for(var i = 0; i <= totalLetters.length-1; i++) {
+    if (vow.includes(totalLetters[i])) {
+      return totalVows.push("-").length;
+    }
+  }
+};
+Journal.prototype.consoCount = function() {
+  var totalVowels =  this.body.split("");
+  for (var i=0; i < totalVowels.length; i++){
+    if (!["a", "i", "u", "e", "o", "."].includes(totalVowels[i])) {
+      return totalVowels[i].length; 
+    }
+  }
+};
+Journal.prototype.getTeaser = function() {
+  return this.body.split(" ").splice(0, 7);
+};
